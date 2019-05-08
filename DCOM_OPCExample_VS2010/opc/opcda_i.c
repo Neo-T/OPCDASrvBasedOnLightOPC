a3ea0ade494d99cf619094132e238b7a6549bda0
Neo-T <marsstory99@hotmail.com>
2019-05-08 08:49:02 +0800

    nothing to do

--- DCOM_OPCExample_VS2010/opc/opcda_i.c
+++ DCOM_OPCExample_VS2010/opc/opcda_i.c
@@ -5,18 +5,18 @@
 /* link this file in with the server and any clients */
 
 
- /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue May 07 20:40:57 2019
+ /* File created by MIDL compiler version 6.00.0366 */
+/* at Wed Jul 09 16:49:48 2008
  */
-/* Compiler settings for ..\opc\opcda.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
-    protocol : dce , ms_ext, c_ext, robust
+/* Compiler settings for .\opcda.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run)
+    protocol : dce , ms_ext, c_ext
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
          DECLSPEC_UUID(), MIDL_INTERFACE()
 */
-/* @@MIDL_FILE_HEADING(  ) */
+//@@MIDL_FILE_HEADING(  )
 
 #pragma warning( disable: 4049 )  /* more than 64k source lines */
 
